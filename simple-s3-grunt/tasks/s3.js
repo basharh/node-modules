ecfb7660db4f686e07a4a109f0bb834305d9b4d4
Bashar Harfoush <a5e3b3@yahoo.com>
2014-03-22 14:16:27 -0400

    Read bucket name from config and validate it

--- simple-s3-grunt/tasks/s3.js
+++ simple-s3-grunt/tasks/s3.js
@@ -13,26 +13,30 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('s3cmdSync', 's3cmd-powered dir-to-bucket sync', function () {
 
-    var done = this.async(),
-        files = this.files;
+    var bucketRE = /^[a-z0-9.-]{3,63}$/,
+        done = this.async(),
+        files = this.files,
+        srcDirArr = files && files[0] && files[0].src,
+        bucket = this.options()['bucket'];
 
-    var srcDirArr = files && files[0] && files[0].src;
-
-    if ( srcDirArr.length != 1 ) {
+    if ( !srcDirArr || srcDirArr.length != 1 ) {
       grunt.fail.fatal('Please specify one existing directory to upload!');
     }
 
-    var cmd = 's3cmd sync --delete-removed ' + srcDirArr[0] + '/* s3://test-sdk091983';
+    if ( !bucket || !bucketRE.test( bucket ) ) {
+      grunt.fail.fatal('Invalid bucket name in task options.');
+    }
+
+    var cmd = 's3cmd sync --delete-removed ' + srcDirArr[0] + '/* s3://' + bucket;
 
     var child = exec(cmd, function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
+        console.log('s3cmd: ' + stdout);
         if (error !== null) {
           grunt.fail.fatal('exec error: ' + error);
           done( false );
         }
         done( true );
     });
-
   });
 
 }
